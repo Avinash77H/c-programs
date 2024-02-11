6de5ad743e2de8facc44cf3189a6c79c36298586
Avinash77H <avinashrakholiya2@gmail.com>
2024-02-11 10:35:00 +0530

    first commit

---
+++ basic/find_factor_of_enter_number_by_user.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,sum=0;
+    cout<<"enter value of n:";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        if(n%i==0 ){
+            cout<<"your factor:"<<i<<endl;
+           
+        }
+        
+         
+    }
+    return 0;
+}
+    
+  
