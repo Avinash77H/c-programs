b38dadddbbea4058b7aa6b57ddf73e72ef9e39f6
Avinash77H <avinashrakholiya2@gmail.com>
2024-02-28 12:16:31 +0530

    28/02/2024

--- bank_project.cpp
+++ bank_project.cpp
@@ -1,107 +1,217 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-void employee1(){
-    int choise;
-    cout<<"employee name :avinash"<<"      "<<"balance :5000"<<endl;
-    cout<<endl<<endl;
-    cout<<"1.withdraw"<<endl;
-    cout<<"2.diposit"<<endl;
-    cout<<"3.check balance"<<endl;
-    cout<<"0.exit"<<endl;
-    cout<<endl<<endl;
-    cout<<"enter your choise:";
-    cin>>choise;
-    switch (choise){
-        case 001:
-        withdrowl();
-        break;
-        case 002:
-        diposit();
-        break;
-        case 003:
-        check_balance();
-        break;
-        case 004:
-        exit(0);
-    }
-    
 
-}
-void employee2(){
-    cout<<"employee name :milan"<<"      "<<"balance :2000"<<endl;
-    int choise;
-    cout<<endl<<endl;
-    cout<<"1.withdraw"<<endl;
-    cout<<"2.diposit"<<endl;
-    cout<<"3.check balance"<<endl;
-    cout<<"0.exit"<<endl;
-    cout<<endl<<endl;
-    cout<<"enter your choise:";
-    cin>>choise;
-}
-void employee3(){
-    cout<<"employee name :ugam"<<"      "<<"balance :7000"<<endl;
-    int choise;
-    cout<<endl<<endl;
-    cout<<"1.withdraw"<<endl;
-    cout<<"2.diposit"<<endl;
-    cout<<"3.check balance"<<endl;
-    cout<<"0.exit"<<endl;
-    cout<<endl<<endl;
-    cout<<"enter your choise:";
-    cin>>choise;
-}
-void employee4(){
-    cout<<"employee name :hardik"<<"      "<<"balance :1000"<<endl;
-    int choise;
-    cout<<endl<<endl;
-    cout<<"1.withdraw"<<endl;
-    cout<<"2.diposit"<<endl;
-    cout<<"3.check balance"<<endl;
-    cout<<"0.exit"<<endl;
-    cout<<endl<<endl;
-    cout<<"enter your choise:";
-    cin>>choise;
-}
-void employee5(){
-    cout<<"employee name :parth"<<"      "<<"balance :4000"<<endl;
-    int choise;
-    cout<<endl<<endl;
-    cout<<"1.withdraw"<<endl;
-    cout<<"2.diposit"<<endl;
-    cout<<"3.check balance"<<endl;
-    cout<<"0.exit"<<endl;
-    cout<<endl<<endl;
-    cout<<"enter your choise:";
-    cin>>choise;
-}
-void withdrowl(){
-
-}
-void diposit(){
-
-}
-void check_balance(){
-    
-}
-int main(){
+int main()
+{
     int input;
-    cout<<"enter account number:";
-    cin>>input;
-    if(input==001){
-        employee1();
+    cout << "enter account number:";
+    std::cin >> input;
+    if (input == 001)
+    {
+        
+        string name = "avinash";
+
+        int balance = 5000;
+        int choise;
+        int withdrowl;
+        int diposit;
+
+        cout << "employee name :" << name << '\t' << "balance :" << balance << endl;
+        cout << "1.withdraw" << endl;
+        cout << "2.diposit" << endl;
+        cout << "3.check balance" << endl;
+        cout << "4.exit" << endl;
+
+        for (int i = 1; i < 10; i++)
+        {
+            cout << "enter your choise:";
+            std::cin >> choise;
+            switch (choise)
+            {
+            case 1:
+
+                cout << "enter withdrowl amount:";
+                std::cin >> withdrowl;
+                balance = balance - withdrowl;
+                break;
+            case 2:
+                cout << "enter diposit amount:";
+                std::cin >> diposit;
+                balance = balance + diposit;
+                break;
+            case 3:
+                cout << "your balance is:" << balance << endl;
+                break;
+            case 4:
+                cout << "thank you for bank visit";
+                exit(0);
+            }
+        }
     }
-    else if(input==002){
-        employee2();
+
+    else if (input == 002)
+    {
+         string name = "milan";
+
+        int balance = 10000;
+        int choise;
+        int withdrowl;
+        int diposit;
+
+        cout << "employee name :" << name << '\t' << "balance :" << balance << endl;
+        cout << "1.withdraw" << endl;
+        cout << "2.diposit" << endl;
+        cout << "3.check balance" << endl;
+        cout << "4.exit" << endl;
+
+        for (int i = 1; i < 10; i++)
+        {
+            cout << "enter your choise:";
+            std::cin >> choise;
+            switch (choise)
+            {
+            case 1:
+
+                cout << "enter withdrowl amount:";
+                std::cin >> withdrowl;
+                balance = balance - withdrowl;
+                break;
+            case 2:
+                cout << "enter diposit amount:";
+                std::cin >> diposit;
+                balance = balance + diposit;
+                break;
+            case 3:
+                cout << "your balance is:" << balance << endl;
+                break;
+            case 4:
+                cout << "thank you for bank visit";
+                exit(0);
+            }
+        }
     }
-     else if(input==003){
-        employee3();
+    else if (input == 003)
+    {
+         string name = "ugam";
+
+        int balance = 7999;
+        int choise;
+        int withdrowl;
+        int diposit;
+
+        cout << "employee name :" << name << '\t' << "balance :" << balance << endl;
+        cout << "1.withdraw" << endl;
+        cout << "2.diposit" << endl;
+        cout << "3.check balance" << endl;
+        cout << "4.exit" << endl;
+
+        for (int i = 1; i < 10; i++)
+        {
+            cout << "enter your choise:";
+            std::cin >> choise;
+            switch (choise)
+            {
+            case 1:
+
+                cout << "enter withdrowl amount:";
+                std::cin >> withdrowl;
+                balance = balance - withdrowl;
+                break;
+            case 2:
+                cout << "enter diposit amount:";
+                std::cin >> diposit;
+                balance = balance + diposit;
+                break;
+            case 3:
+                cout << "your balance is:" << balance << endl;
+                break;
+            case 4:
+                cout << "thank you for bank visit";
+                exit(0);
+            }
+        }
     }
-     else if(input==004){
-        employee4();
+    else if (input == 004)
+    {
+         string name = "hardik";
+
+        int balance = 17000;
+        int choise;
+        int withdrowl;
+        int diposit;
+
+        cout << "employee name :" << name << '\t' << "balance :" << balance << endl;
+        cout << "1.withdraw" << endl;
+        cout << "2.diposit" << endl;
+        cout << "3.check balance" << endl;
+        cout << "4.exit" << endl;
+
+        for (int i = 1; i < 10; i++)
+        {
+            cout << "enter your choise:";
+            std::cin >> choise;
+            switch (choise)
+            {
+            case 1:
+
+                cout << "enter withdrowl amount:";
+                std::cin >> withdrowl;
+                balance = balance - withdrowl;
+                break;
+            case 2:
+                cout << "enter diposit amount:";
+                std::cin >> diposit;
+                balance = balance + diposit;
+                break;
+            case 3:
+                cout << "your balance is:" << balance << endl;
+                break;
+            case 4:
+                cout << "thank you for bank visit";
+                exit(0);
+            }
+        }
     }
-     else if(input==005){
-        employee5();
+    else if (input == 005)
+    {
+         string name = "parth";
+
+        int balance = 2000;
+        int choise;
+        int withdrowl;
+        int diposit;
+
+        cout << "employee name :" << name << '\t' << "balance :" << balance << endl;
+        cout << "1.withdraw" << endl;
+        cout << "2.diposit" << endl;
+        cout << "3.check balance" << endl;
+        cout << "4.exit" << endl;
+
+        for (int i = 1; i < 10; i++)
+        {
+            cout << "enter your choise:";
+            std::cin >> choise;
+            switch (choise)
+            {
+            case 1:
+
+                cout << "enter withdrowl amount:";
+                std::cin >> withdrowl;
+                balance = balance - withdrowl;
+                break;
+            case 2:
+                cout << "enter diposit amount:";
+                std::cin >> diposit;
+                balance = balance + diposit;
+                break;
+            case 3:
+                cout << "your balance is:" << balance << endl;
+                break;
+            case 4:
+                cout << "thank you for bank visit";
+                exit(0);
+            }
+        }
     }
 
     return 0;
