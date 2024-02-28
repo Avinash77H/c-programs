c52bbc044484feba21ac1b053253c85ed0791096
Avinash77H <avinashrakholiya2@gmail.com>
2024-02-28 18:34:33 +0530

    28/02/2024

--- file managment/basic_code.cpp
+++ file managment/basic_code.cpp
@@ -1,16 +1,28 @@
 #include<iostream>
-#include<fstream>
+#include<fstream>  // header file use file managment 
 using namespace std;
 int main(){
+
+
+    // 3 useful classes
+
+    // 1.fstreambase
+    // 2.ifstream->derived from fstreambase
+    // 3.ofstream->derived from fstreambase
     string st1="avinash bhai";
     string st2;
-   /* //opening files using constructor and writing it
-    ofstream out("sample1.txt");//write operation
-    out<<st1;*/
+    //opening files using constructor and writing it
+     //write operation
 
-    //opening files using constructor and reading it
-    ifstream in("sample1.txt");
-    in>>st2;
+    // ofstream x("sample1.txt");   // hear ofstream is a class and 
+    // x<<st1;                   // x is object of class ofstream
+
+    // //opening files using constructor and reading it
+    ifstream in("sample2.txt");
+    //in>>st2; 
+    // also write  and this way benifitial
+    getline(in,st2);
+    cout<<st2;
 
 
     return 0;
